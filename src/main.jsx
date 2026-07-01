f7bf8b53a1f6c95dbeed5346f1968109bdb90538
Iyad Eltifi <iyad@eltifi.com>
2026-07-01 17:28:23 -0400

    Install simple win page copy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { installSimpleCopy } from './simpleCopy'
+
+installSimpleCopy()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
